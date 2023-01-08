8eb6b503b56fdf86cf4c5446874a3622e163c3f7
smtan <tan.stevenmitchell@gmail.com>
2023-01-08 13:48:20 +0100

    Plots apart in plot_main omdat we denken dat het de gym environment breekt

--- Observation.py
+++ Observation.py
@@ -1,9 +1,8 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from Obstacle import Obstacle
 from Robot import Robot
 from Static import Static
 import numpy as np
-# from IPython.display import display, clear_output
 
 
 class Observation:
@@ -121,41 +120,3 @@ class Observation:
                     color = "blue"
 
                 self.ax3 = self.obstacles[i].plot_position_info(self.ax3, color)
-
-
-# Basic 2D simulation. Je kan de simulatie update cycle en orca update cycle aanpassen. Ook kunnen er meer robots toegevoegd of aangepast worden
-# Zie de comment onderaan de loop voor het veranderen van het simulatie gedrag
-# if __name__ == "__main__":
-#     simulation_cycle = 0.01
-#     orca_update_cycle = 1.0 # Verander deze waarde naar 1.0 voor een betere simulatie
-#     ob = Observation(orca_update_cycle, simulation_cycle)
-#
-#     # Add robots to the simulations
-#     speed = 5.0
-#     radius = 1
-#
-#     ob.add_robot3(np.array([5, 5, 0]), radius, np.array([-speed, -speed, 0]), np.array([-5, -5, 0]), 0)
-#     ob.add_robot3(np.array([-5, -5, 0]), radius, np.array([speed, speed, 0]), np.array([5, 5, 0]), 1)
-#     ob.add_robot3(np.array([-5, 5, 0]), radius, np.array([speed, -speed, 0]), np.array([5, -5, 0]), 2)
-#     ob.add_robot3(np.array([5, -5, 0]), radius, np.array([-speed, speed, 0]), np.array([-5, 5, 0]), 3)
-#
-#     for t in np.arange(0, 5, simulation_cycle):
-#         new_positions = []
-#
-#         for i in range(len(ob.obstacles)):
-#             robot = ob.obstacles[i]
-#             print(robot.toString())
-#             new_positions.append(robot.get_position() + robot.get_current_velocity() * simulation_cycle)
-#
-#         ob.update_positions(new_positions)
-#
-#         if t % orca_update_cycle == 0:
-#             new_velocities = ob.orca_cycle()
-#             ob.update_velocities(new_velocities)
-#             ob.update_orca_plot()
-#
-#         ob.update_position_plot()
-#
-#         plt.show(block=False)  # TODO: als False veranderd loopt de simulatie door!!!
-#         clear_output(wait=True)
-#         plt.pause(0.001)
